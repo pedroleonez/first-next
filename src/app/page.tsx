8249e9b1da0d674939c0bc97370912c67fc369fb
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-26 16:02:41 -0300

    feat: add metadata to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home - Projetinho Next.js",
+  description: "This is the home page of the Projetinho Next.js",
+  keywords: "home, projetinho, next.js",
+  openGraph: {
+    title: "Home - Projetinho Next.js",
+    description: "This is the home page of the Projetinho Next.js",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    }
+  }
+}
+
 export default function Home() {
   return (
     <div>
