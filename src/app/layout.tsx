dc92a746e97eaf96d176de43cd2e54ea59ff377e
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-26 16:02:23 -0300

    feat: add metadata to all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,26 @@
 
 import { Header } from "@/components/header";
 import "./globals.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Projetinho Next.js",
+  description: "This is some page of the Projetinho Next.js",
+  keywords: "projetinho, next.js",
+  openGraph: {
+    title: "Projetinho Next.js",
+    description: "This is some page of the Projetinho Next.js",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    }
+  }
+}
 
 
 export default function RootLayout({
