1e9aee3d82630222d8cbf300c820b6e2be53daec
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-26 14:42:10 -0300

    feat: create header component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,14 @@
+export function Header() {
+    return(
+        <header>
+            <h1>Header</h1>
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/dashboard">Dashboard</a></li>
+                    <li><a href="/contacts">Contacts</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
