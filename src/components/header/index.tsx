5ef4314a493e2411997a370e6e9d1b6f4644615e
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-26 16:02:59 -0300

    feat: update header h1

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export function Header() {
     return(
         <header className="bg-gray-900 text-white p-4 flex justify-between items-center h-14">
-            <h1>Header</h1>
+            <h1>Projetinho Next.js</h1>
             <nav>
                 <ul className="flex space-x-4">
                     <li><Link href="/">Home</Link></li>
